ef2e7fc12b9ac8b27b2eddc6454daf04fadb598c
Lucas Loureiro <lucas-loureiro@outlook.com>
2024-03-13 21:02:34 -0300

    feat: change lumpSum label text

--- src/app/painel/clientes/[id]/page.tsx
+++ src/app/painel/clientes/[id]/page.tsx
@@ -491,7 +491,7 @@ export default function ClientDetailsPage() {
 
                       <DetailsRow>
                         <InputContainer size="w-1/2">
-                          <Label htmlFor="lumpSum">Valor Fixo</Label>
+                          <Label htmlFor="lumpSum">Valor do Boleto</Label>
                           <CurrencyInput
                             { ...form.register("lumpSum") }
                             className="flex h-9 w-full rounded-md border border-input bg-white px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
@@ -658,7 +658,7 @@ export default function ClientDetailsPage() {
         </DetailsRow>
 
         <DetailsRow>
-          <DetailsField label="Valor Fixo" value={clientDetailed?.lumpSum} width="min-w-52 w-full" />
+          <DetailsField label="Valor do Boleto" value={clientDetailed?.lumpSum} width="min-w-52 w-full" />
           <DetailsField label="Valor Unitário" value={clientDetailed?.unitValue} width="min-w-52 w-full" />
           <DetailsField label="URL do Contrato">
             <Link
--- src/app/painel/clientes/cadastrar-cliente/page.tsx
+++ src/app/painel/clientes/cadastrar-cliente/page.tsx
@@ -219,7 +219,7 @@ export default function RegisterClient() {
 
           <DetailsRow>
             <InputContainer size="w-1/3">
-              <Label htmlFor="lumpSum">Valor Fixo</Label>
+              <Label htmlFor="lumpSum">Valor do Boleto</Label>
               <CurrencyInput
                 { ...form.register("lumpSum") }
                 className="flex h-9 w-full rounded-md border border-input bg-white px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50"
